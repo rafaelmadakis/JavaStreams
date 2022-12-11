c4a733342702d8ef54a0e3d44a742a1d0ec4a355
Rafael Madakis <rafaelmadakis@gmail.com>
2022-12-11 12:11:06 -0300

    IntStream Iterate

--- src/test/java/com/amigoscode/lectures/Lecture2.java
+++ src/test/java/com/amigoscode/lectures/Lecture2.java
@@ -44,6 +44,11 @@ public class Lecture2 {
   @Test
   public void intStreamIterate() throws Exception {
 
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(number -> number % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
+
   }
 
 
